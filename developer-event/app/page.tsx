66b12a841339da6b1e580ef2d51c0eafc33b223f
lenomed <lenomedmega@gmail.com>
2026-06-24 00:35:33 +0100

    chore

--- developer-event/app/page.tsx
+++ developer-event/app/page.tsx
@@ -1,4 +1,10 @@
+import EventCard from "@/components/eventcard"
 import ExploreBtn from "@/components/Explore"
+
+const events = [
+  { image: "/images/event1.png", title: "Event 1" },
+  { image: "/images/event2.png", title: "Event 2" },
+];
 
 const Page=()=>{
   return(
@@ -7,8 +13,10 @@ const Page=()=>{
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
       <ExploreBtn/>
       <div className="mt-20 space-y-7">
-        {[1,2,3,4,5].map((event)=>(
-          <li key={event}> Event{event}</li>
+        {events.map((event)=>(
+         <li key={event.title}>s
+          <EventCard {...event}/>
+         </li>
         ))}
       </div>
     </section>
